59266e57725705cfa0ea405b6432a75c9362febd
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2025-01-11 23:06:18 +0530

    Login, SignUp APIs, JWT & Bcrypt

--- server/index.js
+++ server/index.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const userRoutes = require('./Routes/userRoutes')
+
 dotenv.config();
 
-const connectToMongo = require('./db');
+const connectToMongo = require('./config/db');
 connectToMongo();
  
 const PORT = process.env.PORT;
 const chats = require('./data');
- 
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("I am the Danger!");
 });
 
-app.get('/api/chats', (req, res) => {
-    res.send(chats);
-});
-
-app.get('/api/chats/:id', (req, res) => {
-    const id = req.params.id;
-    const singleChat = chats.find((c) => c._id === id);
-    res.send(singleChat);
-});
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => console.log("Jesse Pinkman Bitch!"));
